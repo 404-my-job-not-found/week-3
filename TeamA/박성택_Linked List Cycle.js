de16a1b276895d75bba45186cde4f322ddb0e100
stoic-park <demige79@gmail.com>
2025-07-08 14:06:48 +0900

    solve: add solution

--- TeamA/박성택_Linked List Cycle.js
+++ TeamA/박성택_Linked List Cycle.js
@@ -24,18 +24,42 @@ var hasCycle = function(head) {
     // linked list는 값이 중복되지 않는가..?
 
     // let prev = null;
-    let curr = head;
+    // let curr = head;
 
-    const seens = new Set();
+    // const seens = new Set();
 
-    while(curr !== null) {
-        if(seens.has(curr)) {
-            return true; // 있으면 주기가 있는 것이야..?
-        }
-        seens.add(curr);      
-        curr = curr.next;          
-    }
+    // while(curr !== null) {
+    //     if(seens.has(curr)) {
+    //         return true; // 있으면 주기가 있는 것이야..?
+    //     }
+    //     seens.add(curr);      
+    //     curr = curr.next;          
+    // }
     
-    return false; // 끝까지 같다면 주기가 없는 것
+    // return false; // 끝까지 같다면 주기가 없는 것
+
+
+    // Follow up에 "공간 O(1)로 풀어라"가 붙으면 이 방법을 써야 함.
+
+    // 아이디어
+    // 느린 포인터(slow): 한 칸씩 이동
+    // 빠른 포인터(fast): 두 칸씩 이동
+
+    // 만약 사이클이 있다면, 둘은 언젠가 만난다!
+    // 사이클이 없다면 fast가 null에 도달한다.
+
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;         // 1칸 이동
+        fast = fast.next.next;    // 2칸 이동
+
+        if (slow === fast) {
+            return true; // 만남 => 사이클 있음
+        }
+    }
+
+    return false; // fast가 끝까지 감 => 사이클 없음
 
 };
