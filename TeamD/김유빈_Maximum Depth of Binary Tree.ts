9e128e84a54cf2746da44ef7878049d161ce9c70
luwbe1 <luwbe1@naver.com>
2025-07-08 22:09:13 +0900

    feature: 이진 트리 깊이

---
+++ TeamD/김유빈_Maximum Depth of Binary Tree.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) {
+    return 0;
+  }
+
+  const leftDepth = maxDepth(root.left); // 왼쪽 트리의 깊이 (재귀)
+  const rightDepth = maxDepth(root.right); // 오른쪽 트리의 깊이 (재귀)
+
+  return Math.max(leftDepth, rightDepth) + 1;
+}
